5c9d672441bde26b189a48ea950c09339fbe0f28
Colten Bayles <cbayles@outlook.com>
2023-10-02 20:05:48 -0400

    added looping through a slice

--- the_slice.py
+++ the_slice.py
@@ -1,14 +1,21 @@
-#The Players
-players = ["Charlie", "Jason", "Derick", "Paul", "Simon"]
+# #The Players
+# players = ["Charlie", "Jason", "Derick", "Paul", "Simon"]
 
-#Slicing from the beginning of a list
-print(players[0:3])
+# #Slicing from the beginning of a list
+# print(players[0:3])
 
-# Another way to slice from the beginning of a list
-print(players[:4])
+# # Another way to slice from the beginning of a list
+# print(players[:4])
 
-#Slicing the middle of a list
-print(players[2:4])
+# #Slicing the middle of a list
+# print(players[2:4])
 
-#If you want to slice to the end of a list
-print(players[-4:])
+# #If you want to slice to the end of a list
+# print(players[-4:])
+
+#Looping through a slice using a for loop to loop through a subset of te elements within a list
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print("Here are the two ice giants of our Solar System.")
+for planet in planets[-6:]:
+    print(planet.title())
