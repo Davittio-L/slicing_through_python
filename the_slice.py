38cf7824d68c55d0d41636e1e182125c7033badf
Colten Bayles <cbayles@outlook.com>
2023-10-02 20:01:59 -0400

    Simple slicing examples

---
+++ the_slice.py
@@ -0,0 +1,14 @@
+#The Players
+players = ["Charlie", "Jason", "Derick", "Paul", "Simon"]
+
+#Slicing from the beginning of a list
+print(players[0:3])
+
+# Another way to slice from the beginning of a list
+print(players[:4])
+
+#Slicing the middle of a list
+print(players[2:4])
+
+#If you want to slice to the end of a list
+print(players[-4:])
